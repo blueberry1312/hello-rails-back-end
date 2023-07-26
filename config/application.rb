e127926042d2db9ba2d203f93ddad631471d3470
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-26 13:46:13 -0400

    Add linters.yml, setup cors

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module HelloRailsBackEnd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # Esto permitirá todas las solicitudes desde cualquier origen (cambia '*' por el dominio de tu aplicación frontend si quieres limitar el acceso a un dominio específico).
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
